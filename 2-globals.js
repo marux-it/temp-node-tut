e908a28634c1131458f4e7b0c5e74377ee5f74a6
marux <me@marux.it>
2021-10-18 14:51:43 +0200

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+//Globals - No Window !!!!
+
+// __dirname - path to currente directory
+// __fileame - file name
+// require - functione to use modules (CommonJS)
+// module - info about the curente module (file)
+// process - info about environment where the program is being executed
+
+console.log(__dirname)
+
+setInterval(()=> {
+    console.log('hello world')
+}, 1000)
